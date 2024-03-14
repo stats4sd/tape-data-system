c47071b752b7f0b263f56adc4dac546879d16154
Dave Mills <d.e.mills@stats4sd.org>
2024-03-14 19:22:21 +0000

    update npm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/app/theme.css'],
+            refresh: [
+                ...refreshPaths,
+                'app/',
+                'packages/',
+
+            ],
         }),
     ],
 });
