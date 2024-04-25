16af6b18c9a040658ac73f29f5d3be8883ccdd80
Dave Mills <d.e.mills@stats4sd.org>
2024-04-25 18:36:03 +0100

    WIP

--- app/Filament/App/Resources/XlsformResource/RelationManagers/SubmissionsRelationManager.php
+++ app/Filament/App/Resources/XlsformResource/RelationManagers/SubmissionsRelationManager.php
@@ -16,7 +16,7 @@ class SubmissionsRelationManager extends RelationManager
 
     public function isReadOnly(): bool
     {
-        return false;
+        return false;$this->getSelectedTableRecords()
     }
 
     public function form(Form $form): Form
