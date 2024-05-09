2efc005cbf96972f2d99084fac4302de5b9b0130
Dave Mills <d.e.mills@stats4sd.org>
2024-05-09 12:25:29 +0100

    hotfix

--- app/Models/LookupTables/Crop.php
+++ app/Models/LookupTables/Crop.php
@@ -11,12 +11,18 @@ class Crop extends LookupEntry
 
     public function getCsvContentsForOdk(?WithXlsforms $team = null): array
     {
+
+        if ($team) {
+            $isRelevant = $this->isRemoved($team) ? 0 : 1;
+        } else {
+            $isRelevant = null;
+        }
+
         return [
             'id'  => $this->id,
             'name' => $this->name,
             'label' => $this->label,
-            'is_in_context' => $team ? $this->isRemoved($team) : null,
-
+            'is_in_context' => $isRelevant,
         ];
     }
 }
