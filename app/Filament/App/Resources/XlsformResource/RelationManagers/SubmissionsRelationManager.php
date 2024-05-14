21d9a47df9f6b2499f2e10521bfb4bac4c7f0358
Dan Tang <dan@stats4sd.org>
2024-05-14 13:35:56 +0100

    fix typo

--- app/Filament/App/Resources/XlsformResource/RelationManagers/SubmissionsRelationManager.php
+++ app/Filament/App/Resources/XlsformResource/RelationManagers/SubmissionsRelationManager.php
@@ -79,7 +79,7 @@ class SubmissionsRelationManager extends RelationManager
                 // Error occurred for asking "team" relationship in Submission model
                 // I will work on removing previously saved entities, entity_values, custom tables records first, will get back to it later
                 //
-                // Question: If popup modal works fine to edit a submission, why it does not work when we should the Edit page in a separate page...?
+                // Question: If popup modal works fine to edit a submission, why it does not work when we show the Edit page in a separate page...?
                 Tables\Actions\EditAction::make()->label('Edit page')
                     ->url(fn (Submission $record): string => SubmissionResource::getUrl('edit', ['record' => $record])),
 
