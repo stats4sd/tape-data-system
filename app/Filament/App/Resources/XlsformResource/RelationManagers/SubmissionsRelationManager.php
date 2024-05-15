634624f49796e0cf09b1d4b67f37ec56d0bb101b
Dave Mills <d.e.mills@stats4sd.org>
2024-05-15 13:56:50 +0100

    re-enable TAPE-specific values

--- app/Filament/App/Resources/XlsformResource/RelationManagers/SubmissionsRelationManager.php
+++ app/Filament/App/Resources/XlsformResource/RelationManagers/SubmissionsRelationManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\App\Resources\XlsformResource\RelationManagers;
 
+use App\Models\LookupTables\Enumerator;
 use Filament\Forms;
 use Filament\Tables;
 use Filament\Forms\Form;
@@ -45,26 +46,26 @@ class SubmissionsRelationManager extends RelationManager
             ->columns([
                 Tables\Columns\TextColumn::make('odk_id'),
                 Tables\Columns\TextColumn::make('submitted_at'),
-                // Tables\Columns\TextColumn::make('enumerator')
-                // ->getStateUsing(function ($record) {
-                //     $enumeratorId = $record->content['survey_start']['inquirer_choice'];
-                //     if($enumeratorId === "77") {
-                //         return $record->content['survey_start']['inquirer_text'];
-                //     }
-                //     return Enumerator::firstWhere('name', $record->content['survey_start']['inquirer_choice']) ?? '~not found~';
-                // }),
-                // Tables\Columns\TextColumn::make('farm_name')
-                //     ->getStateUsing(function ($record) {
-                //         return $record->content['reg']['farm_name'];
-                //     }),
-                // Tables\Columns\TextColumn::make('respondent_available')
-                //     ->getStateUsing(function ($record) {
-                //         return $record->content['reg']['respondent_check']['respondent_available'];
-                //     }),
-                // Tables\Columns\TextColumn::make('consent')
-                //     ->getStateUsing(function ($record) {
-                //         return $record->content['consent_grp']['consent'] === "1" ? "Yes" : "No";
-                //     }),
+                 Tables\Columns\TextColumn::make('enumerator')
+                 ->getStateUsing(function ($record) {
+                     $enumeratorId = $record->content['survey_start']['inquirer_choice'];
+                     if($enumeratorId === "77") {
+                         return $record->content['survey_start']['inquirer_text'];
+                     }
+                     return Enumerator::firstWhere('name', $record->content['survey_start']['inquirer_choice']) ?? '~not found~';
+                 }),
+                 Tables\Columns\TextColumn::make('farm_name')
+                     ->getStateUsing(function ($record) {
+                         return $record->content['reg']['farm_name'];
+                     }),
+                 Tables\Columns\TextColumn::make('respondent_available')
+                     ->getStateUsing(function ($record) {
+                         return $record->content['reg']['respondent_check']['respondent_available'];
+                     }),
+                 Tables\Columns\TextColumn::make('consent')
+                     ->getStateUsing(function ($record) {
+                         return $record->content['consent_grp']['consent'] === "1" ? "Yes" : "No";
+                     }),
             ])
             ->filters([
                 //
