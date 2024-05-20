0fde27df9e6d8aee6b8a822e837e92861264a05c
Dave Mills <d.e.mills@stats4sd.org>
2024-05-20 17:09:30 +0100

    update hotfix for migration

--- database/migrations/2024_05_20_135542_update_performance_tables.php
+++ database/migrations/2024_05_20_135542_update_performance_tables.php
@@ -14,69 +14,102 @@ return new class () extends Migration {
         Schema::table('performance_activities', function (Blueprint $table) {
             $table->dropForeign('performance_activities_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_activities', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_animals', function (Blueprint $table) {
             $table->dropForeign('performance_animals_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_animals', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_animal_products', function (Blueprint $table) {
             $table->dropForeign('performance_animal_products_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_animal_products', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_crops', function (Blueprint $table) {
             $table->dropForeign('performance_crops_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_crops', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_crop_products', function (Blueprint $table) {
             $table->dropForeign('performance_crop_products_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_crop_products', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_chemical_pesticides', function (Blueprint $table) {
             $table->dropForeign('performance_chem_pest_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_chemical_pesticides', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_machines', function (Blueprint $table) {
             $table->dropForeign('performance_machines_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_machines', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_organic_pesticides', function (Blueprint $table) {
             $table->dropForeign('performance_org_pest_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_organic_pesticides', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_youth_emigrants', function (Blueprint $table) {
             $table->dropForeign('performance_youth_emigrants_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_youth_emigrants', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_youth_females', function (Blueprint $table) {
             $table->dropForeign('performance_youth_females_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
+        });
+
+        Schema::table('performance_youth_females', function (Blueprint $table) {
             $table->foreignId('main_survey_id')->nullable()->change();
         });
 
         Schema::table('performance_youth_males', function (Blueprint $table) {
             $table->dropForeign('performance_youth_males_performance_assessment_id_foreign');
             $table->renameColumn('performance_assessment_id', 'main_survey_id');
-            $table->foreignId('main_survey_id')->nullable()->change();
         });
-    }
+
+        Schema::table('performance_youth_males', function (Blueprint $table) {
+            $table->foreignId('main_survey_id')->nullable()->change();
+        }});
+
 
     /**
      * Reverse the migrations.
