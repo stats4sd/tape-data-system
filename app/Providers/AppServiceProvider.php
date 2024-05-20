b6621abdb22d041f36e446484ac635ac0b6fddf8
Dave Mills <d.e.mills@stats4sd.org>
2024-05-20 11:17:31 +0100

    hotfix for password update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     {
         // unguard all models
         \Eloquent::unguard();
+
+        // set default password requirement to 10 characters to match ODK Central's default
+        \Illuminate\Validation\Rules\Password::defaults(static function () {
+            return Password::min(10);
+        });
+
     }
 }
