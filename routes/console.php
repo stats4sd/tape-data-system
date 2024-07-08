d6a29b16604336578ad8a819576637ea337dc247
Dan Tang <dan@stats4sd.org>
2024-07-08 11:15:05 +0100

    Add sentry crons monitor

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 use Stats4sd\FilamentOdkLink\Commands\PollForOdkData;
 
-Schedule::command(PurgeTelescopeEntries::class)->daily();
-Schedule::command(PollForOdkData::class)->everyFiveMinutes();
+Schedule::command(PurgeTelescopeEntries::class)->daily()->sentryMonitor();
+Schedule::command(PollForOdkData::class)->everyFiveMinutes()->sentryMonitor();
