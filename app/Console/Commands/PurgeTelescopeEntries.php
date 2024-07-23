148250131da77f8a9f23e5ef5d8c4ef861b7ffd5
Dave Mills <d.e.mills@stats4sd.org>
2024-07-23 15:00:03 +0100

    fix typo

--- app/Console/Commands/PurgeTelescopeEntries.php
+++ app/Console/Commands/PurgeTelescopeEntries.php
@@ -4,19 +4,30 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class PurgeTelescopeEntries extends Command
 {
     protected $signature = 'app:purge-telescope-entries';
 
-    protected $description = 'Command description';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes telescope entries older than 1 week and optimises the tables to reclaim disk space.';
 
-    public function handle(): void
+    /**
+     * Execute the console command.
+     */
+    public function handle()
     {
-        $this->info('Purging Telescope entries from more than 1 week ago...');
+        // prune anything older than 1 week;
+        Artisan::call('telescope:prune --hours 168');
 
-        Artisan::call('telescope:prune --hours=168');
-
-        $this->info('Telescope entries purged!');
+        // optimise the tables to reclaim disk space (usually not needed; included here because the first time this is run there might be actual GBs of data to clean up / reclaim);
+        DB::statement('OPTIMIZE TABLE telescope_entries');
+        DB::statement('OPTIMIZE TABLE telescope_entries_tags');
+        DB::statement('OPTIMIZE TABLE telescope_monitoring');
     }
 }
